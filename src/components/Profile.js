2b7a47cc087901773075a2de7b73a1e3f92e1014
viviannove0731 <viviannove0731@gmail.com>
2018-09-20 19:19:22 +0800

    update profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -112,7 +112,7 @@ class Profile extends React.Component {
           </Grid>
           <Grid item xs={12} sm={12}>
           <TextField
-            required
+            requiredgit pull url master
             id="description"
             name="description"
             label="原密碼"
