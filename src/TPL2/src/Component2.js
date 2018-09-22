8127dd5530bc7cd3bf1fa2b56e647e48fb0007a7
Katherinehsing <chaling1997@gmail.com>
2018-09-22 14:47:53 +0800

    template2

---
+++ src/TPL2/src/Component2.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+import img_elImage from './images/Component2_elImage_405622.jpg';
+import img_elImageCopy4 from './images/Component7_elImageCopy_1027808.jpg';
+import img_elImageCopy from './images/Component7_elImageCopy_1027808.jpg';
+import img_elImageCopy2 from './images/Component7_elImageCopy_1027808.jpg';
+
+
+export default class Component2 extends Component {
+
+  // This component doesn't use any properties
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    let baseStyle = {};
+    // eslint-disable-next-line no-unused-vars
+    let layoutFlowStyle = {};
+    
+    const style_image = {
+        backgroundImage: 'url('+img_elImage+')',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '50% 50%',
+        backgroundSize: 'cover',
+        pointerEvents: 'none',
+     };
+    const style_imageCopy4 = {
+        backgroundImage: 'url('+img_elImageCopy4+')',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '50% 50%',
+        backgroundSize: 'cover',
+        pointerEvents: 'none',
+     };
+    const style_imageCopy = {
+        backgroundImage: 'url('+img_elImageCopy+')',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '50% 50%',
+        backgroundSize: 'cover',
+        pointerEvents: 'none',
+     };
+    const style_imageCopy2 = {
+        backgroundImage: 'url('+img_elImageCopy2+')',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '50% 50%',
+        backgroundSize: 'cover',
+        pointerEvents: 'none',
+     };
+    
+    return (
+      <div className="Component2" style={baseStyle}>
+        <div className="background">
+          <div className='elImage' style={style_image} />
+        </div>
+        <div className="foreground">
+          <div className='elImageCopy4' style={style_imageCopy4} />
+          <div className='elImageCopy' style={style_imageCopy} />
+          <div className='elImageCopy2' style={style_imageCopy2} />
+        </div>
+      </div>
+    )
+  }
+  
+
+}
---
+++ src/TPL2/src/Page8Screen.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './App.css';
+import Component8 from './Component8';
+import btn_icon_back_page8 from './images/btn_icon_back_page8.png';
+
+// UI framework component imports
+import Button from 'muicss/lib/react/button';
+import Appbar from 'muicss/lib/react/appbar';
+import Container from 'muicss/lib/react/container';
+
+
+export default class Page8Screen extends Component {
+
+  // Properties used by this component:
+  // appActions, deviceInfo
+
+  onClick_elButton = (ev) => {
+    // Go to screen 'Page.9'
+    this.props.appActions.goToScreen('page9', { transitionId: 'fadeIn' });
+  
+  }
+  
+  
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    let baseStyle = {};
+    // eslint-disable-next-line no-unused-vars
+    let layoutFlowStyle = {};
+    if (this.props.transitionId && this.props.transitionId.length > 0 && this.props.atTopOfScreenStack && this.props.transitionForward) {
+      baseStyle.animation = '0.25s ease-in-out '+this.props.transitionId;
+    }
+    if ( !this.props.atTopOfScreenStack) {
+      layoutFlowStyle.height = '100vh';
+      layoutFlowStyle.overflow = 'hidden';
+    }
+    
+    const style_background = {
+        width: '100%',
+        height: '100%',
+     };
+    const style_background_outer = {
+        backgroundColor: '#f6f6f6',
+        pointerEvents: 'none',
+     };
+    const style_button = {
+        display: 'block',
+        textAlign: 'center',
+     };
+    const style_button_outer = {
+        cursor: 'pointer',
+     };
+    
+    return (
+      <Container fluid={true} className="AppScreen Page8Screen" style={baseStyle}>
+        <div className="background">
+          <div className='appBg containerMinHeight elBackground' style={style_background_outer}>
+            <div style={style_background} />
+          
+          </div>
+          
+        </div>
+        <div className="layoutFlow" style={layoutFlowStyle}>
+          <div className='hasNestedComps elComponent'>
+            <div>
+              <Component8 appActions={this.props.appActions} deviceInfo={this.props.deviceInfo} locStrings={this.props.locStrings} />
+            </div>
+          
+          </div>
+          
+          <div className='actionFont elButton' style={style_button_outer}>
+            <Button style={style_button}  color="accent" onClick={this.onClick_elButton} >
+              {this.props.locStrings.screen18_button_25210}
+            </Button>
+          
+          </div>
+          
+        </div>
+        <Appbar className="navBar">
+          <div className="title">Page.8</div>  <div className="backBtn" onClick={ (ev)=>{ this.props.appActions.goBack() } }><img src={btn_icon_back_page8} alt="" style={{width: '50%'}} /></div>
+        </Appbar>
+        
+      </Container>
+    )
+  }
+  
+
+}
