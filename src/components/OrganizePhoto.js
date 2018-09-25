032fc21518d2ca1e5929173a66bbcb9e2a9c835d
viviannove0731 <viviannove0731@gmail.com>
2018-09-25 20:18:49 +0800

    connect with face recognition engine

--- src/components/OrganizePhoto.js
+++ src/components/OrganizePhoto.js
@@ -55,8 +55,8 @@ class OrganizePhoto extends React.Component {
   constructor(){
     super();
     this.state = {
-      images: [["https://cdn.minephoto.tw/image/photo/70add93be6e841ea9943391cfb2ba503T5m3w1R3.jpg", "https://cdn.minephoto.tw/image/photo/0f9bf6ae5f5245b4a3beafd8d439c9d02E6zJA88.jpg", "https://cdn.minephoto.tw/image/photo/1d3440c2e1cf4871ab991a7d274d90f73c35Wa23.jpg", "https://cdn.minephoto.tw/image/photo/cd63bb0fab52468c9d0618f3bfbc79a3f92P70P2.jpg"]],
-      members: ['https://cdn.minephoto.tw/image/photo/70add93be6e841ea9943391cfb2ba503T5m3w1R3.jpg', "https://cdn.minephoto.tw/image/photo/0f9bf6ae5f5245b4a3beafd8d439c9d02E6zJA88.jpg", "https://cdn.minephoto.tw/image/photo/1d3440c2e1cf4871ab991a7d274d90f73c35Wa23.jpg", "https://cdn.minephoto.tw/image/photo/cd63bb0fab52468c9d0618f3bfbc79a3f92P70P2.jpg"],
+      images: [],
+      members: [],
       numOfPic: 0,
       order: 0,
       imageData: []
@@ -125,7 +125,7 @@ class OrganizePhoto extends React.Component {
             <ImagePicker 
               multiple
               onPick={(image) => this.setState({numOfPic: image.length})}
-              images={imagesOfSomeone.map((image, i) => ({src: image, value: i}))}
+              images={imagesOfSomeone.map((image, i) => ({src: image.replace('uploads', 'static'), value: i}))}
             />
             <Divider/>
           </div>
@@ -140,7 +140,18 @@ class OrganizePhoto extends React.Component {
       "memoryProjectId": this.props.bookId,
       "order" : value
     }).then( (res)=> {
-      this.setState({imageData: res.data});
+      if(this.state.order === 2){
+        let images = [];
+        let members = [];
+
+        for(let data in res.data){
+          images.push( res.data[data]);
+          members.push('/' + data.split('[No]')[0])
+        }
+        this.setState({images, members})
+      }else{
+        this.setState({imageData: res.data});
+      }
     });
     this.setState({order: value})
   };
